e4c945f4346f7e28eab86b3dd35e154184bb7485
재윤 <gojun1352@gmail.com>
2022-09-13 10:48:34 +0900

    220913
    플레이어 스크립트

---
+++ Zombie/Assets/Script/Gamemanager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    public static Gamemanager instance;
+    public bool isgameover = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
