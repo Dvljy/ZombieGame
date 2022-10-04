dbdf2c4b08a94ed90a6e698a7ebb1fad17bfb328
재윤 <gojun1352@gmail.com>
2022-10-04 10:49:41 +0900

    221004
    플레이어가 총 잡는 모션처리

---
+++ Assets/1.Scripts/PlayerShooter.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class PlayerShooter : MonoBehaviour
+{
+    public Gun gun;
+    public Transform gunPivot;
+    public Transform leftHandMount;
+    public Transform rightHandMount;
+
+    private PlayerInput playerInput;
+    private Animator playerAnimator;
+
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        gun.gameObject.SetActive(true);
+    }
+
+    private void OnDisable()
+    {
+        gun.gameObject.SetActive(false);
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    void UpdateUi()
+    {
+        if (gun != null && UImanager.instance != null)
+        {
+            UImanager.instance.UpdateAmmoText(gun.magAmmo, gun.ammoRemain);
+        }
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        //총의 기준점 gunPivot을 3D 모델의 오른쪽 팔꿈치 위치로 이동
+        gunPivot.position = playerAnimator.GetIKHintPosition(AvatarIKHint.RightElbow);
+
+        //IK를 사용하여 왼손의 위치와 회전을 총의 왼쪽 손잡이에 맞춤
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1.0f);
+
+        playerAnimator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandMount.position);
+        playerAnimator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandMount.rotation);
+
+        //IK를 사용하여 오른손의 위치와 회전을 총의 오른쪽 손잡이에 맞춤
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1.0f);
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f);
+
+        playerAnimator.SetIKPosition(AvatarIKGoal.RightHand, rightHandMount.position);
+        playerAnimator.SetIKRotation(AvatarIKGoal.RightHand, rightHandMount.rotation);
+    }
+}
